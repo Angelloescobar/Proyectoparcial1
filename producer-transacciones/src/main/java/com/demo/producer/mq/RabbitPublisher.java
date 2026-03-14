d716e21e6fa8a27518bff5a6cf19123e9250381a
Angelloescobar <aescobarg21@miumg.edu.gt>
2026-03-14 15:55:34 -0600

    proyecto finalizado

--- producer-transacciones/src/main/java/com/demo/producer/mq/RabbitPublisher.java
+++ producer-transacciones/src/main/java/com/demo/producer/mq/RabbitPublisher.java
@@ -1,7 +1,10 @@
 package com.demo.producer.mq;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.demo.producer.model.Transaccion;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -24,16 +27,45 @@ public class RabbitPublisher {
         this.channel = connection.createChannel();
     }
 
-    public void publicar(String nombreCola, String mensajeJson) throws Exception {
-        channel.queueDeclare(nombreCola, true, false, false, null);
+    public void publicar(String nombreCola, String mensajeJson, Transaccion transaccion) throws Exception {
+
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-max-priority", 10);
+
+        channel.queueDeclare(nombreCola, true, false, false, args);
+
+        int prioridad = calcularPrioridad(transaccion);
 
         AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
                 .contentType("application/json")
                 .deliveryMode(2)
+                .priority(prioridad)
                 .build();
 
         channel.basicPublish("", nombreCola, props, mensajeJson.getBytes(StandardCharsets.UTF_8));
-        System.out.println("Enviado a cola: " + nombreCola);
+
+        String tipoPrioridad = prioridad >= 8 ? "ALTA" : "NORMAL";
+
+        System.out.println("Enviado a cola: " + nombreCola
+                + " | TX: " + transaccion.idTransaccion
+                + " | Prioridad: " + tipoPrioridad
+                + " (" + prioridad + ")");
+    }
+
+    private int calcularPrioridad(Transaccion transaccion) {
+        try {
+            String numero = transaccion.idTransaccion.replaceAll("\\D", "");
+            int valor = Integer.parseInt(numero);
+
+            if (valor % 2 == 0) {
+                return 9; 
+            } else {
+                return 4; 
+            }
+
+        } catch (Exception e) {
+            return 4;
+        }
     }
 
     public void cerrar() throws Exception {
